ae28363eecb44a3da6c7e3f4e24e1a9e39b9da76
Souhaib Chadidi <souhaibchadidi02@gmail.com>
2020-12-02 10:26:31 +0100

    😪

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import './App.css';
 
 import Navbar from './Components/Navbar'
 import Contact from './Components/Contact'
